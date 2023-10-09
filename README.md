02842a1a70a7cdf599152fa0071762fec5841ac5
Abhishek Patel <abhishelpatel171019@gmail.com>
2023-10-09 12:16:28 +0530

    Added readme and comments to understand code

---
+++ README.md
@@ -0,0 +1,56 @@
+# express-routes-lister
+
+### Introduction
+All of backend developer who are using express, they are facing problem to list all the Api routes with methods in one place. And we can take reference to call at the time of calling the Api routes. So **express-routes-lister** is the solution for this problem that is easy to use.
+
+### How to use it
+
+Just install the package in your project using npm
+
+```
+npm i express-routes-lister
+```
+
+
+And after installing you just want to import it into your main/starter file. 
+```JavaScript
+const getRoutesList = require('express-routes-lister');
+```
+
+And call the **getRoutesList** function by passing just your app of your express.
+
+After making all the required routes on your main/stater file, you have to call the getRoutesList() function to list all the routes. If your other route also come from other file it will detect that routes also and print it with proper path.
+
+```JavaScript
+// Requiring express
+
+const express = require('express');
+const { getRoutesList } = require('express-routes-lister');
+
+const PORT = process.env.PORT;
+
+const app = express();
+
+app.get('/', (req, res) => {
+    return res.status(200).send("Home Page");
+});
+
+app.get('/service', (req, res) => {
+    return res.status(200).send("Service Page");
+});
+
+app.post('/signup', (req, res) => {
+    return res.status(200).send("SignUp Page");
+});
+
+getRoutesList(app);
+
+app.listen(PORT, () => {
+    console.log(`Server started running on port ${PORT}`);
+});
+
+```
+
+Code output:
+
+[![Screenshot-2023-10-09-120723.png](https://i.postimg.cc/wvRhDG1K/Screenshot-2023-10-09-120723.png)](https://postimg.cc/WFjFvSmS)
--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 function print(path, layer) {
+
+    // If we arrived at that layer that has key route then split the path than concat it with path and call the next print to get into the level of method
     if (layer.route) {
         layer.route.stack.forEach(print.bind(null, path.concat(split(layer.route.path))))
-    } else if (layer.name === 'router' && layer.handle.stack) {
+    } 
+    
+    // If we arrived at that layer that has property name and its value is route that means it has more route layer inside layer.handle.stack
+    // And in that type of layer it does not have route but it has regexp tha has route path so split it using regexp and concat it with path
+    else if (layer.name === 'router' && layer.handle.stack) {
         layer.handle.stack.forEach(print.bind(null, path.concat(split(layer.regexp))))
-    } else if (layer.method) {
+    } 
+    
+    // In this layer if layer is having a key method than print it and if 
+    else if (layer.method) {
         console.log('|%s\t | /%s\t',
             layer.method.toUpperCase(),
             path.concat(split(layer.regexp)).filter(Boolean).join('/'))
@@ -12,11 +21,20 @@ function print(path, layer) {
 }
 
 function split(thing) {
+
+    // If type of route is string than split them with / and make array
     if (typeof thing === 'string') {
-        return thing.split('/')
-    } else if (thing.fast_slash) {
+        return thing.split('/');
+
+    }
+
+    // It is specifically for catch_all routes * type route that will catch all the routes
+    else if (thing.fast_slash) {
         return ''
-    } else {
+    }
+    
+    // Here thing will update using regex
+    else {
         var match = thing.toString()
             .replace('\\/?', '')
             .replace('(?=\\/|$)', '$')
@@ -25,11 +43,13 @@ function split(thing) {
             ? match[1].replace(/\\(.)/g, '$1').split('/')
             : '<complex:' + thing.toString() + '>'
     }
+
 }
 
 function getRoutesList(app) {
     const stack = app._router.stack;
     const path = [];
+    console.log('----------------------------------------');
     stack.forEach(layer => {
         print(path, layer);
     });
@@ -37,4 +57,4 @@ function getRoutesList(app) {
 
 module.exports = {
     getRoutesList
-}
+};
